349aa13d9dcab6fc72a995a207647ffd5b9892ba
doomslace <doomslace@gmail.com>
2021-05-08 22:48:34 -0500

    Finished project

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I have a window but tube tires! Oh no.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
